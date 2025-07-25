84f98b13e6aa0ba625593655684d2a10652a6114
Pardhesh Maddala <148302780+pardhesh@users.noreply.github.com>
2025-07-25 21:46:13 +0530

    Update embed_store.py

--- embed_store.py
+++ embed_store.py
@@ -12,7 +12,7 @@ with open("split_chunks.pkl", "rb") as f:
 os.environ["HF_TOKEN"]= os.getenv("HF_TOKEN")
 embedding_model= HuggingFaceEmbeddings(model_name= "all-MiniLM-L6-v2")
 
-st.secrets["HF_TOKEN"]= os.getenv("HF_TOKEN")
+
 vectorstore =FAISS.from_documents(chunks,embedding_model)
 
 FAISS.save_local(vectorstore,"faiss_db")
