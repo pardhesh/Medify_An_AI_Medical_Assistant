d3c01875c9118d8f7fe35ccb3a337a52678a7869
Pardhesh Maddala <148302780+pardhesh@users.noreply.github.com>
2025-07-25 20:15:39 +0530

    Add files via upload

--- embed_store.py
+++ embed_store.py
@@ -3,16 +3,16 @@ from langchain_huggingface import HuggingFaceEmbeddings
 import pickle
 import os
 from dotenv import load_dotenv
-
+import streamlit as st
 load_dotenv()
 
 with open("split_chunks.pkl", "rb") as f:
     chunks = pickle.load(f)
 
-
+os.environ["HF_TOKEN"]= os.getenv("HF_TOKEN")
 embedding_model= HuggingFaceEmbeddings(model_name= "all-MiniLM-L6-v2")
 
-
+st.secrets["HF_TOKEN"]= os.getenv("HF_TOKEN")
 vectorstore =FAISS.from_documents(chunks,embedding_model)
 
 FAISS.save_local(vectorstore,"faiss_db")
